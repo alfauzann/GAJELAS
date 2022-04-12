fe1891e2c248db5b86172406f2efc23287f42416
alfauzann <alfauzan@student.telkomuniversity.ac.id>
2022-04-13 00:25:23 +0700

    first commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login.index');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email:dns',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+
+        return back()->with([
+            'login' => 'Login tidak berhasil'
+        ]);
+        return redirect()->intended('/login');
+    }
+}
